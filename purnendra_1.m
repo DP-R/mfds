3594eecb0f662eda203ecfb1ae2545408e0d8a72
mightycandle <sashank.n.1711@gmail.com>
2021-06-06 14:36:09 +0530

    fix small print format

--- purnendra_1.m
+++ purnendra_1.m
@@ -23,8 +23,8 @@ c=17
 d=19
 fprintf('Assume that the arrival of people in a 24/7 vaccination centre follows a poisson distribution. Assume a typical demographic distribution of \n')
 fprintf('0/0 dividend\n %d 0-18 \n %d 18-45 \n %d 45-60 \n %d above 60',a,b,c,d)
-fprintf('Also the arrival rate of each age group at the vaccination centre is proportional to population percentage normalised to 10 perhr')
-fprintf('\nprobability that in an interval of 1 hr the older ones get more vaccines than their younger ones .The highest vaccination status given only 13 per hr')
+fprintf('\nAlso,the arrival rate of each age group at the vaccination centre is proportional to population percentage normalised to 10 per hour.')
+fprintf('\nThe probability that in an interval of 1 hr, the older ones get more vaccines than their younger ones .The highest vaccination status given only 13 per hr')
 lambda_sol=num2cell([a,b,c,d]/10)
 [a,b,c,d]=lambda_sol{:}
 double(P(7,1,a)*P(3,1,b)*P(2,1,c)*P(1,1,d))+double(P(6,1,a)*P(4,1,b)*P(2,1,c)*P(1,1,d))+double(P(5,1,a)*P(4,1,b)*P(3,1,c)*P(1,1,d))
