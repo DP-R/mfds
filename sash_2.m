ea8f993e0f3b8bfba91af9d4ab36f54baecf2cf4
mightycandle <sashank.n.1711@gmail.com>
2021-06-09 22:07:40 +0530

    updated solution with eig

--- sash_2.m
+++ sash_2.m
@@ -10,8 +10,11 @@ final=zeros(1,3);
 for i = 1:3
 	final(i)=-10+randi(20);
 end
-fprintf("A housefly in a room, travels to the point (%d, %d, %d), and disorients its path by an angle of %d degrees with respect to the positive X-axis, and finally reaches the point (%d, %d, %d) with respect to this new frame. What would this new coordinate be, when read by the initial coordinate frame ? \n(Consider the starting point of the housefly as the origin in the initial frame)\n\n",initial(1),initial(2),initial(3),angle_degs,final(1),final(2),final(3));
-fprintf("\nOptions:\n");
+fprintf("A housefly in a room, travels to the point (%d, %d, %d), and disorients its path by an angle of %d degrees with respect to the positive X-axis, and finally reaches the point P(%d, %d, %d) with respect to this new frame.\n",initial(1),initial(2),initial(3),angle_degs,final(1),final(2),final(3));
+fprintf("Let this final coordinate, when read by the initial coordinate frame, be Q(x,y,z). ");
+fprintf("Which of the following matrices has eigenvalues equal to the coordinates of Q?\n")
+fprintf("(Consider the starting point of the housefly as the origin in the initial frame)\n\n");
+fprintf("\n\tOptions:\n");
 Id=["A.","B.","C.","D."];
 
 rot=get_rot(angle);
@@ -22,26 +25,31 @@ result=get_result(AB,final);
 for i = 1:4
 	now=zeros(1,3);
 	for j = 1:3
-		now(j)=-5+randi(10)+randi(999)/10000;
+		now(j)=-5+randi(10)+randi(9999)/10000;
 	end
 	if i == 4
 		now=result;
 	end
-	fprintf("%s (%.4f, %.4f, %.4f) \n",Id(i),now(1),now(2),now(3));
+	now_diag=diag(now);
+	r=rand(3); %random_matrix
+	% option matrix = inv(r)*diag(eig_values)*r
+	final=inv(r)*now_diag*r;
+	fprintf("%s\n",Id(i));
+	disp(final);
 end
-fprintf("\nAnswer: D.\n Explanation:\n\n");
+fprintf("\nAnswer: D.\nExplanation:\n\n");
 fprintf("Let us name the initial frame as A, and the final frame (after translation and rotation) as B.\n");
-fprintf("Firstly we form the rotation matrix.\nIt is given as: (for an angle 'x' , wrt Positive X-axis)\n\n");
-fprintf("1     0     0     0  \n");
-fprintf("1  cos(x) -sin(x) 0  \n");
-fprintf("1  sin(x)  cos(x) 0  \n");
-fprintf("0     0     0     1\n\n");
-fprintf('Now, for translation by a vector "T(x,y,z)":, translation matrix:\n\n');
-fprintf("1   0   0   T(x) \n");
-fprintf("0   1   0   T(y) \n");
-fprintf("0   0   1   T(z) \n");
-fprintf("0   0   0    1 \n\n");
-fprintf("We now form the matrix A/B by multiplying the rotation and translation matrices, which is: \n\n");
+fprintf("Firstly we formulate the rotation matrix.\nIt is given as: (for an angle 'x' , wrt Positive X-axis)\n\n");
+fprintf("\t1     0     0     0  \n");
+fprintf("\t1  cos(x) -sin(x) 0  \n");
+fprintf("\t1  sin(x)  cos(x) 0  \n");
+fprintf("\t0     0     0     1\n\n");
+fprintf('Now, for translation by a vector "T(x,y,z)":\ntranslation matrix:\n\n');
+fprintf("\t1   0   0   T(x) \n");
+fprintf("\t0   1   0   T(y) \n");
+fprintf("\t0   0   1   T(z) \n");
+fprintf("\t0   0   0    1 \n\n");
+fprintf("We now form the matrix A/B by multiplying the rotation and translation matrices, which is: \n");
 AB
 fprintf("\nSo, the new position (P) in old frame(A) can be expressed as:\n\t (P in A) = (A/B) * (P in B)\n");
 fprintf("Where , P in B :- coordinates of final point in the new frame, represented as a 4*1 matrix :-\n");
@@ -51,8 +59,9 @@ for i = 1:3
 end
 p_in_b(4,1)=1;
 p_in_b
-fprintf("\nUpon multiplying AB and p_in_b, we get a 4*1 matrix, and our answer is first three row elements\n");
+fprintf("\nUpon multiplying AB and p_in_b, we get a 4*1 matrix, and our answer is first three elements\n");
 result
+fprintf("Out of the given options, Option-D is a matrix that has these same eigenvalues.\n");
 
 % Functions used while solving:
 function rot = get_rot(angle)
