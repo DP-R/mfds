0ea12c79961e8a9211db51b6e4431d01577c3978
mightycandle <sashank.n.1711@gmail.com>
2021-06-12 22:47:23 +0530

    edit final

--- Bonus/Group_25.m
+++ Bonus/Group_25.m
@@ -6,6 +6,8 @@ for i = 1:5
     q1(1,i);
 end
 
+q2(2,i);
+
 function variant(x,y)
     fprintf("\n\n\n");
     fprintf("<Q. %d, V. %d>\n----------------------------------------------\n",x,y);
@@ -131,4 +133,19 @@ function q1(ques,vari)
             
             out =X;
         end
+end
+
+function q2(ques,vari)
+    variant(ques,vari);
+    fprintf('As part of an experiment to determine the efficiency of a vaccine, a test drive is conducted and certain parameters were measured during the drive like gender,age and weight\n')
+    fprintf('Determine what kind of data they were?\n')
+    answer=['nominal interval ratio'];
+    dupe1=['ordinal','interval', 'ordinal'];
+    dupe2=['ratio','interval', 'ordinal'];
+    dupe3=['nominal','ratio', 'ordinal'];
+    fprintf('A: %s\n',answer)
+    fprintf('B: %s\n',dupe1)
+    fprintf('C: %s\n',dupe2)
+    fprintf('D: %s\n',dupe3)
+
 end
