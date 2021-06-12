251fd0cde1923e890827cc911a63c32147abf0d7
Datturi Purnendra Reddy <purnendradatturi@gmail.com>
2021-06-12 22:25:54 +0530

    some stupid mcq for bonus marks

---
+++ Bonus/purnendra_2.m
@@ -0,0 +1,10 @@
+fprintf('Ss part of an experiment to determine the efficacy of a vaccine a test drive is conducted and certain parameters were measured during the drive like gender,age and weight\n')
+fprintf('Determine what kind of data they were?\n')
+answer=['nominal interval ratio'];
+dupe1=['ordinal','interval', 'ordinal'];
+dupe2=['ratio','interval', 'ordinal'];
+dupe3=['nominal','ratio', 'ordinal'];
+fprintf('A: %s\n',answer)
+fprintf('B: %s\n',dupe1)
+fprintf('C: %s\n',dupe2)
+fprintf('D: %s\n',dupe3)
